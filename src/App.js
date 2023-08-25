43a925e3991f1b9b5d3e7565231bb1f488146c5f
blessingsmart <bublessingsmart@gmail.com>
2023-08-25 02:02:51 +0100

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { NavBar } from "./components/NavBar";
 import Trainings from "./components/Trainings";
-import { About } from "./components/about";
+import { About } from "./components/About";
 import { Home } from "./components/Home";
 import { AboutMain } from "./Pages/AboutMain";
-import { Contact } from "./components/contact";
+import { Contact } from "./components/Contact";
 import { Testimonials } from "./components/Testimonials";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
