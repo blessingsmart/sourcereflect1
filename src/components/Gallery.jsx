625a7bba75433706034fc39f31dfb08adff61d29
blessingsmart <bublessingsmart@gmail.com>
2023-08-27 04:31:21 +0100

    modified:   src/components/Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 const Slider = ({ sliderData }) => {
@@ -8,10 +8,11 @@ const Slider = ({ sliderData }) => {
   const autoScroll = true;
   const intervalTime = 5000;
 
-  const nextSlide = () => {
+  // Wrap nextSlide and auto functions with useCallback
+  const nextSlide = useCallback(() => {
     setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
     console.log("next");
-  };
+  }, [currentSlide, slideLength]);
 
   const prevSlide = () => {
     setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
@@ -21,9 +22,10 @@ const Slider = ({ sliderData }) => {
   // Use useRef to store the interval reference
   const slideInterval = useRef(null);
 
-  function auto() {
+  // Wrap auto function with useCallback
+  const auto = useCallback(() => {
     slideInterval.current = setInterval(nextSlide, intervalTime);
-  }
+  }, [nextSlide]);
 
   useEffect(() => {
     if (autoScroll) {
@@ -33,32 +35,34 @@ const Slider = ({ sliderData }) => {
     return () => {
       clearInterval(slideInterval.current);
     };
-  }, [autoScroll, currentSlide]);
+  }, [autoScroll, currentSlide, auto]);
 
   return (
     <div className="w-full h-[50vh] relative overflow-hidden">
       <AiOutlineArrowLeft
-        className="arrow prev absolute top-[35%] left-2 text-black border-2 border-black rounded-full cursor-pointer hover:bg-white hover:text-gray-700"
+        className="arrow prev absolute top-[35%] left-2 text-white border-2 border-white rounded-full cursor-pointer hover:bg-white hover:text-gray-700"
         onClick={prevSlide}
       />
       <AiOutlineArrowRight
-        className="arrow next absolute top-[35%] right-2 text-black border-2 border-black rounded-full cursor-pointer hover:bg-white hover:text-gray-700"
+        className="arrow next absolute top-[35%] right-2 text-white border-2 border-white rounded-full cursor-pointer hover:bg-white hover:text-gray-700"
         onClick={nextSlide}
       />
       {sliderData.map((slide, index) => {
         return (
           <div
-            className={`absolute top-0 left-0 w-full h-full transition-all duration-500 ${
-              index === currentSlide ? "opacity-100" : "opacity-0"
+            className={`absolute top-0 left-0 w-full h-full opacity-0 transition-all duration-500 ${
+              index === currentSlide ? "opacity-100" : ""
             }`}
             key={index}
           >
             {index === currentSlide && (
-              <img
-                src={slide.image}
-                alt="slide"
-                className="object-cover w-full h-full"
-              />
+              <div className="relative h-full">
+                <img
+                  src={slide.image}
+                  alt="slide"
+                  className="object-cover w-full h-full"
+                />
+              </div>
             )}
           </div>
         );
