47b645504e5967d5f27435882b65dd26cf28c0ad
blessingsmart <bublessingsmart@gmail.com>
2023-10-04 09:41:49 +0100

    Update navigation bar
    Add logo and make it responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -53,11 +53,11 @@ const links = [
 
   return (
     <div className='flex justify-between drop-shadow-md sm:px-10 items-center w-screen h-16 bg-gradient-to-r from-green-600 to-lime-600 fixed z-20'>
-      <RouterLink to='/' className='flex items-center'>
-        <img src={logo} alt="mainlogo" className="w-10 h-10 mr-2" />
-        <h1 className='text-lg md:text-xl text-white'>SOURCE REFLECT</h1>
-      </RouterLink>
-      <ul className="hidden md:flex">
+        <RouterLink to='/' className='flex items-center'>
+            <img src={logo} alt="mainlogo" className="w-10 h-10 mr-2" />
+            <h1 className='text-lg md:text-xl text-white'>SOURCE REFLECT</h1>
+        </RouterLink>
+        <ul className="hidden md:flex">
             {links.map(({id, link, title, to}) => (
                 <li 
                     key={id} className='px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-200'>
@@ -67,23 +67,21 @@ const links = [
                         smooth="true"
                         duration={500}
                         className="cursor-pointer"
-                        onClick={() => scrollTo(to)}
                         >
                         {title}
                         </ScrollLink>
                     ) : (
-                        <RouterLink to={link} onClick={() => setNav(!nav)}>
+                        <RouterLink to={link}>
                         {title}
                         </RouterLink>
                     )}
                 </li>
             ))}      
-      </ul>
+        </ul>
 
-      <div onClick={() => setNav(!nav)}className='cursor-pointer pr-4 z-10 text-white md:hidden'>
+        <div onClick={() => setNav(!nav)}className='cursor-pointer pr-4 z-10 text-white md:hidden'>
             {nav ? <FaTimes size={30} /> : <FaBars size={30} />}   
         </div>
-
         {nav && (
             <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-r from-green-600 to-lime-600">
 
